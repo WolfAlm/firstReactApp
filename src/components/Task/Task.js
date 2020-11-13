15eaba16068eef69f408e0ff90405ca5e18e8902
WolfAlm <wolfalm16@gmail.com>
2020-11-13 15:41:23 +0300

    Добавление scss и изменение верстки на Flex

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import './Task.css'
+import styles from './Task.module.scss'
+import classnames from 'classnames/bind'
+
+const cs = classnames.bind(styles)
+
 class Task extends React.Component {
 
     printTaskInfo = () => {
@@ -10,16 +14,16 @@ class Task extends React.Component {
 
     render() {
         return (
-            <div className="container">
+            <div className={cs("container", {["container:hover"]: true})}>
                 <header>
-                        <div className='id'>{this.props.id}</div>
-                        <div className='name'>{this.props.name}</div>
+                        <div className={cs("id")}>{this.props.id}</div>
+                        <div className={cs("name")}>{this.props.name}</div>
                 </header>
                 <section>
-                    <div className='description'>{this.props.description}</div>
+                    <div className={cs("description")}>{this.props.description}</div>
                 </section>
                 <footer>
-                    <div className='status'> Status: {this.props.completed ? 'completed' : 'not completed'}</div>
+                    <div className={cs("status")}> Status: {this.props.completed ? 'completed' : 'not completed'}</div>
                     <button onClick={this.printTaskInfo}>Done!</button>
                 </footer>
             </div>
